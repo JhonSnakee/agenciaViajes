33363197583b99b7af1b66efab92e7a84bd8a2d8
Jhon Alejandro Murillo Diaz <56074441+JhonSnakee@users.noreply.github.com>
2024-09-24 19:01:25 -0500

    up: modulo web

--- app/src/main/java/com/poli/agenciaviajes/ui/web/WebViewModel.kt
+++ app/src/main/java/com/poli/agenciaviajes/ui/web/WebViewModel.kt
@@ -1,13 +1,7 @@
 package com.poli.agenciaviajes.ui.web
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class WebViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is Web Fragment"
-    }
-    val text: LiveData<String> = _text
 }
